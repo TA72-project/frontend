0e68365091be9dbc4b2d5ad7acb3dec6adc13917
Valentin271 <36198422+Valentin271@users.noreply.github.com>
2023-11-08 15:13:16 +0100

    feat: login (#2)
    I added the mechanism to login from the login page. You get redirected
    to the dashboard page if login is successful.
    I also added a request wrapper that handles authentication redirection.

--- src/pages/loginPage/loginPage.tsx
+++ src/pages/loginPage/loginPage.tsx
@@ -1,56 +1,106 @@
-import {Button, Grid, Paper, TextField, Typography, IconButton, InputAdornment, FormControl, InputLabel, OutlinedInput} from "@mui/material";
-import { useState } from "react";
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import React, { useState } from "react";
 
-export default function LoginPage(){
-    const [formValues, setFormValues] = useState({
-        email: '',
-        password: ''
-    });
-    const [showPassword, setShowPassword] = useState(false);
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import {
+  Button,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+  IconButton,
+  InputAdornment,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+} from "@mui/material";
+import { request } from "../../utils";
+import { useNavigate } from "react-router-dom";
 
-    return(
-        <Grid container justifyContent="center" alignItems="center" style={{ height: '100vh' }}>
-            <Grid item xs={10} sm={6} md={4} lg={2}>
-                <Paper elevation={3} sx={{ padding:5, borderRadius:'15px' }}>
-                    <Typography variant="h5" sx={{textAlign:'center', mb: 2}}>
-                        Login
-                    </Typography>
-                    <Grid container direction="column" alignItems="center" sx={{gap: 2}}>
-                        <TextField
-                            label="Email"
-                            variant="outlined"
-                            value={formValues.email}
-                            onChange={(e) => setFormValues({...formValues, email: e.target.value})}
-                        />
-                        <FormControl variant="outlined">
-                            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-                            <OutlinedInput
-                                id="outlined-adornment-password"
-                                type={showPassword ? 'text' : 'password'}
-                                onChange={(e)=>setFormValues({...formValues, password: e.target.value})}
-                                endAdornment={
-                                    <InputAdornment position="end">
-                                        <IconButton
-                                            aria-label="toggle password visibility"
-                                            onClick={handleClickShowPassword}
-                                            edge="end"
-                                        >
-                                            {showPassword ? <VisibilityOff /> : <Visibility />}
-                                        </IconButton>
-                                    </InputAdornment>
-                                }
-                                label="Password"
-                            />
-                        </FormControl>
-                        <Button variant="contained" color="primary">
-                            Login
-                        </Button>
-                    </Grid>
-                </Paper>
+export default function LoginPage() {
+  const [formValues, setFormValues] = useState({
+    mail: "",
+    password: "",
+  });
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const navigate = useNavigate();
+
+  const handleLogin = React.useCallback(
+    (e: React.MouseEvent) => {
+      e.preventDefault();
+
+      request.post("/auth/login", formValues).then(() => {
+        navigate("/tableau_de_bord");
+      });
+    },
+    [formValues, navigate],
+  );
+
+  return (
+    <Grid
+      container
+      justifyContent="center"
+      alignItems="center"
+      style={{ height: "100vh" }}
+    >
+      <Grid item xs={10} sm={6} md={4} lg={2}>
+        <Paper elevation={3} sx={{ padding: 5, borderRadius: "15px" }}>
+          <Typography variant="h5" sx={{ textAlign: "center", mb: 2 }}>
+            Login
+          </Typography>
+          <form>
+            <Grid
+              container
+              direction="column"
+              alignItems="center"
+              sx={{ gap: 2 }}
+            >
+              <TextField
+                label="Email"
+                variant="outlined"
+                value={formValues.mail}
+                onChange={(e) =>
+                  setFormValues({ ...formValues, mail: e.target.value })
+                }
+              />
+              <FormControl variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">
+                  Password
+                </InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-password"
+                  type={showPassword ? "text" : "password"}
+                  onChange={(e) =>
+                    setFormValues({ ...formValues, password: e.target.value })
+                  }
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  label="Password"
+                />
+              </FormControl>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                onClick={handleLogin}
+              >
+                Login
+              </Button>
             </Grid>
-        </Grid>
-    )
+          </form>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
 }
