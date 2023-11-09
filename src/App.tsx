9977ec3384e98ddf8c62feb6b79629094dbe8db8
Beladric <kilian.goetz@yahoo.fr>
2023-11-09 22:06:30 +0100

    run fmt

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import {Route, Routes, useLocation} from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import NavBar from "./components/navbar/navBar.tsx";
 import pages from "./datas/pages.tsx";
 import LoginPage from "./pages/loginPage/loginPage.tsx";
 
 export default function App() {
-    const location = useLocation();
-    const showNavBar = location.pathname !== "/";
+  const location = useLocation();
+  const showNavBar = location.pathname !== "/";
 
-    return(
-        <>
-            {showNavBar && <NavBar />}
-            <Routes>
-                {pages.map((element) => (
-                    <Route
-                        key={element.text}
-                        path={"/" + element.path}
-                        element={element.element}
-                    />
-                ))}
-                <Route path="/" element={<LoginPage />} />
-            </Routes>
-        </>
-    )
+  return (
+    <>
+      {showNavBar && <NavBar />}
+      <Routes>
+        {pages.map((element) => (
+          <Route
+            key={element.text}
+            path={"/" + element.path}
+            element={element.element}
+          />
+        ))}
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </>
+  );
 }
