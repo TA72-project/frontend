3a167d7dea9a80e57281d85fd5ecb8523b24eb9e
Valentin271 <36198422+Valentin271@users.noreply.github.com>
2023-11-09 13:47:26 +0100

    chore: added prettier (#3)
    I added prettier, a script to run it and a pipeline.
    
    I let the default configuration, the main point is that we have the same
    style, whatever it is. Feel free to propose better configuration.

--- src/pages/missions/missionsPage.tsx
+++ src/pages/missions/missionsPage.tsx
@@ -1,7 +1,15 @@
-export default function MissionsPage(){
-    return(
-        <div style={{height:'100vh',width:'100%',display:'flex',alignItems:'center',justifyContent:'center'}}>
-            <h1>Missions</h1>
-        </div>
-    )
+export default function MissionsPage() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1>Missions</h1>
+    </div>
+  );
 }
