99a85207f96b22797a028d22fa3b7b24046208ae
Beladric <kilian.goetz@yahoo.fr>
2023-11-12 18:54:26 +0100

    Delete useless code

--- src/utils/formatterUtils.ts
+++ src/utils/formatterUtils.ts
@@ -1,19 +1,3 @@
-import { User } from "../type/model";
-
-export function formatUserName(user: User | null) : string{
-    if(user != null) {
-        return user.fname + " " + user.lname.toUpperCase();
-    }
-    return "";
-}
-
-export function formatUserAvatar(user: User | null) : string{
-    if(user != null) {
-        return user.fname[0].toUpperCase() + user.lname[0].toUpperCase();
-    }
-    return "";
-}
-
 export function formatPositiveInt(value: string | number) : number {
     const intValue = typeof value === 'string' ? parseInt(value) : value;
 
